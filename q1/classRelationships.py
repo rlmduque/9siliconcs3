e688fc277ca04a77e9f21db50a887d2dfac9208e
rlmduque <rlmduque@brc.pshs.edu.ph>
2026-09-24 23:44:01 +0800

    Update classRelationships.py

--- q1/classRelationships.py
+++ q1/classRelationships.py
@@ -1,8 +1,8 @@
 class BankAccount:
     def __init__(self, account_holder, PIN, balance, frozen):
         self.account_holder = account_holder
-        self._PIN = PIN
-        self._balance = balance
+        self.__PIN = PIN
+        self.__balance = balance
         self.frozen = frozen
         self.cards = []
 
